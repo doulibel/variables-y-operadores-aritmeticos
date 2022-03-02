4ee1787cde8ed7418d3e5156e3a7bb7c55781304
doulibel <doulibel@gmail.com>
2022-03-01 22:21:12 -0300

    requerimiento3

---
+++ assests/js/requerimiento3.js
@@ -0,0 +1,4 @@
+console.log("mensaje de prueba3")
+var diasDelAño = prompt("ingresa un numero de dias")
+var diasDelAño = parseInt(diasDelAño);
+console.log("este número corresponde a:" , Math.floor(diasDelAño / 1) , "dias" , Math.floor(diasDelAño / 7) , );
