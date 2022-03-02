29edba6b1120eed952993228efce4392bed7190c
doulibel <doulibel@gmail.com>
2022-03-01 21:53:57 -0300

    requerimiento1

---
+++ assests/js/requerimiento1.js
@@ -0,0 +1,11 @@
+console.log('mensaje de prueba1')
+var datoUno = prompt('ingresa un numero mayor a 0');
+var datoUno = parseInt(datoUno);
+var datoDos = prompt('ingresa un numero mayor que 0 distinto al anterior')
+var datoDos = parseInt(datoDos);
+
+console.log("el resultado de la suma de ambos números es:" , + (datoUno + datoDos));
+console.log("el resultado de la resta de ambos números es:" , + (datoUno - datoDos));
+console.log("el resultado de la multiplicación de ambos números es:" , + (datoUno * datoDos));
+console.log("el resultado de la división de ambos números es:" , + (datoUno / datoDos));
+console.log("el resultado del módulo entre ambos números es:" , + (datoUno % datoDos));
