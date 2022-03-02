30be9ba99c7d1c14ac332f9c5a9608a322623b77
doulibel <doulibel@gmail.com>
2022-03-01 22:14:20 -0300

    requerimiento2

---
+++ assests/js/requerimiento2.js
@@ -0,0 +1,5 @@
+console.log("mensaje de prueba2")
+var temperatura = prompt("ingresa la temperatura en grados celcius")
+var temperatura = parseInt(temperatura);
+console.log("la temperatura en grados kelvin es:" , + (temperatura + 273.15));
+console.log("la temperatura en grados fahrenheit es:" , + (temperatura * (9/5) + 32));
